c404cc65a2840169c6f2dba403158d3a6ee1f1fb
Nadeem Lari <nadeemlari@gmail.com>
2021-11-06 15:21:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsetting.json")
+    .Build();
+
+var conString = config.GetConnectionString("ConString");
+
+Console.Read();
